3be285c987ebd92e0a22f8a6d4e0e5be3d288d94
map32 <freepina@gmail.com>
2014-11-20 13:16:54 -0500

    sdfjsdkfjldj

--- 03WordSearch/WordSearch.java
+++ 03WordSearch/WordSearch.java
@@ -1,13 +1,62 @@
 import java.util.*;
 
 public class WordSearch {
-    static ArrayList<String>[] Grid;
+    private char[][] grid;
+    private int width,length;
+    static String[] words;
+    private char[] concatenated;
 
     public WordSearch(int x, int y) {
-	Grid = new ArrayList<String>[x]();
+	Grid = new char[x][y];
+	width = x;
+	length = y;
+	for (int i=0;i<x;i++) {
+	    for (int j=0;j<y;j++) {
+		grid[i][j] = '#';
+	    }
+	}
+    }
+
+    public void concatenate(String s) {
+	for (int i=0;i<s.length();i++){
+	    concatenated[i] = s.substring(i,i+1);
+	}
+    }
+
+    public void add() {
+	Random r = new Random();
+	while(true){
+	    int i = r.nextInt(words.length);
+	    if (2*words[i].length()>width+length)
+		continue;
+	    if(words[i].length()<length){
+		horizontal(words[i],r.nextInt(2));
+	    }
+	    if(words[i].length()<width){
+		vertical(words[i],r.nextInt(2));
+	    }
+	}
+    }
+    public void vertical();
+    
+    public void horizontal();
+
+    public void diagonal();
+
+    public String toString() {
+	String s = "";
+	for (int i=0;i<width;i++) {
+	    for (int j=0;j<length;j++) {
+		s+=grid[i][j];
+		s+=" ";
+	    }
+	    s+="\n";
+	}
+	return s;
     }
 
     public static void main(String[]df) {
-	System.out.println(Grid.toString());
+	WordSearch map =  new WordSearch(20,20);
+	System.out.println(map.toString());
     }
 }
