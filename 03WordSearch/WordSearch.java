0b02b69ed380bfc31a98388df366d7bdda08e4b1
map32 <freepina@gmail.com>
2014-11-20 12:38:54 -0500

    the first 3rd commit

---
+++ 03WordSearch/WordSearch.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class WordSearch {
+    static ArrayList<String>[] Grid;
+
+    public WordSearch(int x, int y) {
+	Grid = new ArrayList<String>[x]();
+    }
+
+    public static void main(String[]df) {
+	System.out.println(Grid.toString());
+    }
+}
