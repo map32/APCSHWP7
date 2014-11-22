b44a3eefa0ccfcc40b166952a835d7d0a2f23fc5
Dong Shin <freepina@gmail.com>
2014-11-21 23:22:47 -0500

    add function done
    vertical horizontal diagonal yet to be done

--- 03WordSearch/WordSearch.java
+++ 03WordSearch/WordSearch.java
@@ -5,43 +5,67 @@ public class WordSearch {
     private int width,length;
     static String[] words;
     private char[] concatenated;
+    private Random r;
 
-    public WordSearch(int x, int y) {
-	Grid = new char[x][y];
-	width = x;
-	length = y;
-	for (int i=0;i<x;i++) {
-	    for (int j=0;j<y;j++) {
-		grid[i][j] = '#';
+    public WordSearch(int rows, int cols) {
+	grid = new char[rows][cols];
+	length = rows;
+	width = cols;
+	Random r = new Random();
+	clear();
+    }
+
+    public void clear() {
+	for (int i=0;i<length;i++) {
+	    for (int j=0;j<width;j++) {
+		grid[i][j] = ' ';
 	    }
 	}
     }
 
     public void concatenate(String s) {
 	for (int i=0;i<s.length();i++){
-	    concatenated[i] = s.substring(i,i+1);
+	    concatenated[i] = s.charAt(i);
 	}
     }
 
     public void add() {
-	Random r = new Random();
 	while(true){
 	    int i = r.nextInt(words.length);
-	    if (2*words[i].length()>width+length)
+	    if (2*words[i].length()>width+length) {
 		continue;
-	    if(words[i].length()<length){
-		horizontal(words[i],r.nextInt(2));
 	    }
-	    if(words[i].length()<width){
-		vertical(words[i],r.nextInt(2));
+	    if(words[i].length()>length && words[i].length()<=width){
+		horizontal(words[i],r.nextInt(length),xRange(words[i]));
+		continue;
+	    }
+	    if(words[i].length()>width && words[i].length()<=length){
+		vertical(words[i],yRange(words[i]),r.nextInt(width));
+		continue;
+	    }
+	    int a = r.nextInt(3);
+	    if (a==0){
+		horizontal(words[i],r.nextInt(length),xRange(words[i]));
+	    } else if (a==1){
+		vertical(words[i],yRange(words[i]),r.nextInt(width));
+	    } else if (a==2){
+		diagonal(words[i],yRange(words[i]),xRange(words[i]));
 	    }
 	}
     }
-    public void vertical();
     
-    public void horizontal();
+    private int xRange(String s){
+	return r.nextInt(width-s.length()+1);
+    }
+    private int yRange(String s){
+	return r.nextInt(length-s.length()+1);
+    }
 
-    public void diagonal();
+    public void vertical(String s, int rows, int cols){}	
+    
+    public void horizontal(String s, int rows, int cols){}
+    
+    public void diagonal(String s, int rows, int cols){}
 
     public String toString() {
 	String s = "";
