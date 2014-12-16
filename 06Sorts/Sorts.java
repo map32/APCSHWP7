75a1cb178e18e0c4da1ba415942a3e1cda808303
map32 <freepina@gmail.com>
2014-12-16 13:20:28 -0500

    lsdkf

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,10 +1,20 @@
 public class Sorts {
 
+    public static void main(String[] args){
+	int[] c = new int[10];
+	for(int i=0;i<10;i++){
+	    c[i]=10-i;
+	}
+	bubble(c);
+	System.out.println(toString(c));
+    }
+
+
     public static void bubble(int[] c){
 	int temp;
-	for(int i=0;i<length-1;i++){
+	for(int i=0;i<c.length-1;i++){
 	    temp = c[i];
-	    for(int j=i;j<length-i;j++){
+	    for(int j=i;j<c.length-1;j++){
 		if(c[j]>c[j+1]){
 		    c[j]=c[j+1];
 		    c[j+1]=temp;
@@ -15,8 +25,8 @@ public class Sorts {
 
     public static void insertion(int[] c){
 	int temp;
-	for(int i=1;i<length;i++){
-	    temp = array[i];
+	for(int i=1;i<c.length;i++){
+	    temp = c[i];
 	    for(int j=i-1;j>=0;j--){
 		if(c[j]>c[i]){
 		    c[j+1]=c[j];
@@ -27,9 +37,9 @@ public class Sorts {
     }
 
     public static void selection(int[] c){
-	for(int i=0;i<length;i++){
+	for(int i=0;i<c.length;i++){
 	    int min=i;
-	    for(int j=i;j<length;j++){
+	    for(int j=i;j<c.length;j++){
 		if(c[min]>c[j]){
 		    min=j;
 		}
@@ -38,5 +48,15 @@ public class Sorts {
 	    c[min]=c[i];
 	    c[i]=mintemp;
 	}
+    }
+    
+    public static String toString(int[] c) {
+	String str = "[ ";
+	for(int i=0;i<c.length;i++) {
+	    str+=c[i];
+	    str+=" ";
+	}
+	str+="]";
+	return str;
     }
 }
